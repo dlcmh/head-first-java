a08ef608a5e6756c2c0d86864a52da1547110c5c
David Chin <dlcmhd@me.com>
2017-03-23 09:22:11 +0800

    Add 2 classes for: (1) type of object, (2) containing the main method

---
+++ 02-01-first-object/Dog.java
@@ -0,0 +1,11 @@
+class Dog {
+        // instance variables
+        int size;
+        String breed;
+        String name;
+
+        // a method
+        void bark() {
+                System.out.println("Ruff! Ruff!");
+        }
+}
---
+++ 02-01-first-object/DogTestDrive.java
@@ -0,0 +1,7 @@
+class DogTestDrive {
+        public static void main(String[] args) {
+                Dog d = new Dog(); // make a Dog object
+                d.size = 40; // use the dot operator to set the size of the Dog
+                d.bark(); // use the dot operator to call its bark() method
+        }
+}
